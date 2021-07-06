6a9f0db2fb382004e1af6f3714f1e9f63ad828dd
barrytrearty <btrearty@gmail.com>
2021-07-06 17:24:34 +0100

    push before trying class ideas and more bootstrap

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 "strict mode";
+
+const tapestryRow = document.getElementById("tapestry-row");
+const tapestryButton = document.getElementById("tapestry-button");
+
+const displayTapestry = function () {
+  if ((tapestryRow.style.visibility = "hidden")) {
+    tapestryRow.style.visibility = "visible";
+  } else if ((tapestryRow.style.visibility = "visible")) {
+    tapestryRow.style.visibility = "hidden";
+  }
+};
+
+tapestryButton.addEventListener("click", displayTapestry);
